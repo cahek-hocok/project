b608d915b563a275a8e50adf3dc85f550f291b3b
Oleksandr Kozachenko <cahek.hocok@gmail.com>
2021-05-15 01:24:40 +0300

    first commit

---
+++ js/script006.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const person = `5`;
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+//console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+//console.log(obj["name"]);
+ //           0           1             2     3          4  5
+let arr = ['plum.png', 'orange.jpeg', 6, 'apple.bmp', {}, []];
+console.log(arr[0]);
